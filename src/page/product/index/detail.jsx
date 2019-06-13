3f57d367ca9f3e5d48260e0dd25091a6f65d3bc5
honghaitzz11 <honghaitzz11@gmail.com>
2019-06-13 14:07:21 +0800

    暂停更新

--- src/page/product/index/detail.jsx
+++ src/page/product/index/detail.jsx
@@ -2,7 +2,7 @@
  * @Author: Johnhong9527
  * @Date:   2019-06-10 11:15:03
  * @Last Modified by:   Johnhong9527
- * @Last Modified time: 2019-06-11 15:43:43
+ * @Last Modified time: 2019-06-13 14:06:03
  */
 // ProductSavePage
 import React from 'react';
@@ -22,7 +22,8 @@ import {
   Upload,
   Modal,
   Progress,
-  message
+  message,
+  Empty
 } from 'antd';
 import PageTitle from 'component/page-title/index.jsx';
 import HNumber from 'component/h-number/index.jsx';
@@ -53,6 +54,7 @@ class ProductDetail extends React.Component {
     };
     this.getCategoryList = this.getCategoryList.bind(this);
     this.mapPropsToFields = this.mapPropsToFields.bind(this);
+    this.loadProduct = this.loadProduct.bind(this);
     // this.loadData = this.loadData.bind(this);
   }
 
@@ -64,35 +66,35 @@ class ProductDetail extends React.Component {
       stock: res.data.stock
     });*/
     this.loadProduct();
-    this.getCategoryList(0);
+    // this.getCategoryList(0);
   }
-  async loadProduct() {
-    // console.log(this.props.match.params.pid);
-    let getProduct = await _product.getProduct(this.props.match.params.pid);
-    // console.log(getProduct.data.subtitle);
-    this.props.form.setFieldsValue({
-      name: getProduct.data.name,
-      subtitle: getProduct.data.subtitle,
-      price: getProduct.data.price,
-      categoryId: ['12', '1231'],
-      stock: getProduct.data.stock
+
+  loadProduct() {
+    _product.getProduct(this.props.match.params.pid).then(res => {
+      const getProduct = res.data;
+      this.props.form.setFieldsValue({
+        name: getProduct.name,
+        subtitle: getProduct.subtitle,
+        price: getProduct.price,
+        categoryId: [101795],
+        stock: getProduct.stock
+      });
+      const subImages = getProduct.subImages.split(',');
+      const subImagesArr = subImages.map((item, index) => {
+        return {
+          uid: index,
+          name: getProduct.imageHost + item,
+          status: 'done',
+          url: getProduct.imageHost + item,
+          thumbUrl: getProduct.imageHost + item
+        };
+      });
+      this.setState({
+        detail: getProduct.detail,
+        filelist: [...subImagesArr]
+      });
+      this.getCategoryList(0);
     });
-    const subImages = getProduct.data.subImages.split(',');
-    const subImagesArr = subImages.map((item, index) => {
-      // console.log(item);
-      return {
-        uid: index,
-        name: getProduct.data.imageHost + item,
-        status: 'done',
-        url: getProduct.data.imageHost + item,
-        thumbUrl: getProduct.data.imageHost + item
-      };
-    });
-    this.setState({
-      detail: getProduct.data.detail,
-      filelist: [...subImagesArr]
-    });
-    console.log(getProduct.data.imageHost);
   }
 
   getCategoryList(parentCategoryId) {
@@ -100,11 +102,10 @@ class ProductDetail extends React.Component {
       res => {
         if (parentCategoryId === 0) {
           res.data = res.data.slice(1150, 1200);
-          // res.data = res.data.slice(0, 100);
-          res.data.map(key => {
-            key.isLeaf = false;
-            return key;
-          });
+          // res.data.map(key => {
+          //   key.isLeaf = false;
+          //   return key;
+          // });
         }
         this.setState({
           categoryList: res.data
@@ -127,7 +128,7 @@ class ProductDetail extends React.Component {
   };
 
   onSelectChange(data) {
-    // console.log(data);
+    console.log(data);
   }
 
   loadData = selectedOptions => {
@@ -149,7 +150,7 @@ class ProductDetail extends React.Component {
     );
   };
   onCascaderChange(value, selectedOptions) {
-    // console.log(value, selectedOptions);
+    console.log(value, selectedOptions);
     value.forEach(item => {
       if (item !== undefined) {
       }
@@ -203,7 +204,7 @@ class ProductDetail extends React.Component {
   render() {
     const { getFieldDecorator } = this.props.form;
     const { autoCompleteResult, previewVisible, previewImage, filelist } = this.state;
-    console.log(filelist);
+    // console.log(JSON.stringify(filelist));
     const formItemLayout = {
       labelCol: {
         xs: { span: 12 },
@@ -273,10 +274,8 @@ class ProductDetail extends React.Component {
               rules: [{ type: 'array', required: true, message: '情选择商品分类' }]
             })(
               <Cascader
-                disabled={true}
                 options={this.state.categoryList}
                 ref={child => (this.child = child)}
-                // onChange={e => this.onSelectChange(e)}
                 loadData={this.loadData}
                 onChange={e => this.onCascaderChange(e)}
                 fieldNames={{ label: 'name', value: 'id' }}
@@ -284,6 +283,17 @@ class ProductDetail extends React.Component {
                 notFoundContent
               />
             )}
+            <Select defaultValue={102442}>
+              {this.state.categoryList.map(item => (
+                <Option key={item.id}>{item.id}</Option>
+              ))}
+            </Select>{' '}
+            <Select defaultValue={'工具'}>
+              {this.state.categoryList.map(item => (
+                <Option key={item.id}>{item.id}</Option>
+              ))}
+            </Select>
+            <div>{this.state.categoryList.length}</div>
           </FormItem>
           <FormItem label="商品价格">
             {getFieldDecorator('price', {
@@ -296,25 +306,38 @@ class ProductDetail extends React.Component {
             })(<HNumber disabled={true} min={0} placeholder="库存" type="number" tips="件" />)}
           </FormItem>
           <FormItem label="商品图片" {...editFormItemLayout}>
-            <Upload
-              // disabled={true}
-              name="subImages"
-              defaultFileList={filelist}
-              // onPreview={e => this.upHandlePreview(e)}
-              // onChange={e => this.upHandleChange(e)}
-              listType="picture-card"
-              // customRequest={e => this.upCustomRequest(e)}
-              // action="http://adminv2.happymmall.com/manage/product/upload.do"
-            >
-              <div>
-                {/*<Progress percent={30} />*/}
-                <Icon type="plus" />
-                <div className="ant-upload-text">上传图片</div>
-              </div>
-            </Upload>
+            {getFieldDecorator('upload', {
+              valuePropName: 'filelist',
+              getValueFromEvent: this.normFile
+            })(
+              filelist.length > 0 ? (
+                <div>
+                  <Upload
+                    disabled={true}
+                    name="subImages"
+                    onPreview={e => this.upHandlePreview(e)}
+                    fileList={filelist}
+                    listType="picture-card"
+                  />
+                </div>
+              ) : (
+                <span>
+                  <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} />
+                </span>
+              )
+            )}
           </FormItem>
           <FormItem {...editFormItemLayout} label="商品详情">
-            <div>{this.state.detail}</div>
+            {/*
+                <div dangerouslySetInnerHTML={{ __html: this.state.detail }} />
+              */}
+            {this.state.detail !== '' ? (
+              <div>{this.state.detail}</div>
+            ) : (
+              <div>
+                <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} />
+              </div>
+            )}
           </FormItem>
         </Form>
       </div>
